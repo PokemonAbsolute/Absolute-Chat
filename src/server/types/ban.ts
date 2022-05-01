a67d82741937050ee0b45c9c7763648141076df8
Jess <toxocious@gmail.com>
2022-05-01 15:29:02 -0700

    Add type interfaces for Ban and User data

---
+++ src/server/types/ban.ts
@@ -0,0 +1,15 @@
+export default interface BanInterface {
+  User_ID?: number;
+  Banned_By?: number;
+  Banned_On?: number;
+
+  RPG_Ban?: boolean;
+  RPG_Ban_Reason?: string;
+  RPG_Ban_Staff_Notes?: string;
+  RPG_Ban_Until?: number;
+
+  Chat_Ban?: boolean;
+  Chat_Ban_Reason?: string;
+  Chat_Ban_Staff_Notes?: string;
+  Chat_Ban_Until?: number;
+}
---
+++ src/server/types/user.ts
@@ -0,0 +1,5 @@
+export default interface UserInterface {
+  ID: number;
+  Username: string;
+  Rank: string;
+}
