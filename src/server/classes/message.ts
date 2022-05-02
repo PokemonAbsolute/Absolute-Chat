0d83bf16175f93e1de65752210f337ad235e3d92
Jess <toxocious@gmail.com>
2022-05-01 20:38:54 -0700

    Implement basic Message class

---
+++ src/server/classes/message.ts
@@ -0,0 +1,41 @@
+import User from './user';
+
+import MessageInterface from '../types/message';
+
+export default class Message {
+  private message: string;
+  private isPrivate: boolean = false;
+  private sentOn: number;
+  private sentBy?: User | undefined;
+
+  constructor(message: string, sentBy?: User, isPrivate?: boolean) {
+    this.message = message;
+    this.isPrivate = isPrivate ?? false;
+    this.sentBy = sentBy;
+    this.sentOn = Math.floor(Date.now() / 1000);
+  }
+
+  public fromUser(): MessageInterface {
+    return {
+      userID: this.sentBy?.userData?.ID,
+      userName: this.sentBy?.userData?.Username,
+      userRank: this.sentBy?.userData?.Rank,
+      userAvatar: this.sentBy?.userData?.Avatar,
+      messageText: this.message,
+      isPrivate: this.isPrivate,
+      sentOn: this.sentOn,
+    };
+  }
+
+  public fromBot(): MessageInterface {
+    return {
+      userID: -1,
+      userName: 'Absol',
+      userRank: 'Bot',
+      userAvatar: 'images/Avatars/Custom/3.png',
+      messageText: this.message,
+      isPrivate: this.isPrivate,
+      sentOn: this.sentOn,
+    };
+  }
+}
