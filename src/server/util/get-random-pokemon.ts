e23a6041a0afee378ac4e0bccb20a75bb6785689
Jess <toxocious@gmail.com>
2022-05-06 11:32:06 -0700

    Implement utility function to get a random Pokedex entry

---
+++ src/server/util/get-random-pokemon.ts
@@ -0,0 +1,21 @@
+import MySQL from '../classes/mysql';
+
+export const getRandomPokemon = async () => {
+  const MYSQL_QUERY: string =
+    'SELECT `Pokemon`, `Forme` FROM `pokedex` ORDER BY RAND() LIMIT 1';
+  const MYSQL_PARAMS: string[] = [];
+
+  let RANDOM_POKEMON: any[] | undefined;
+  try {
+    RANDOM_POKEMON = await MySQL.doQuery(MYSQL_QUERY, MYSQL_PARAMS);
+  } catch (err) {
+    console.log(
+      '[Util | GetRandomPokemon] Unable to process SQL query:',
+      MYSQL_QUERY,
+      MYSQL_PARAMS,
+      err
+    );
+  }
+
+  return RANDOM_POKEMON;
+};
