fd732ebcdd1a180336b2f8f7b90ec0a802bd9895
Jess <toxocious@gmail.com>
2022-05-28 14:09:18 -0700

    Log sent chat messages to the database

--- src/server/classes/message.ts
+++ src/server/classes/message.ts
@@ -1,12 +1,15 @@
 import User from './user';
+import Log from './log';
 
 import MessageInterface from '../types/message';
+import UserInterface from '../types/user';
 
 export default class Message {
   private message: string;
   private sentOn: number;
   private isPrivate: boolean = false;
   private isPrivateTo: number;
+  private isCommand: boolean = false;
 
   constructor(message: string, isPrivate?: boolean, isPrivateTo?: number) {
     this.message = message;
@@ -16,6 +19,16 @@ export default class Message {
   }
 
   public fromUser(sentBy: User): MessageInterface {
+    if (typeof sentBy.userData !== 'undefined') {
+      Log.toDatabase(
+        this.message,
+        sentBy.userData,
+        this.sentOn,
+        this.isPrivate,
+        this.isPrivateTo
+      );
+    }
+
     return {
       userID: sentBy.userData?.ID,
       userName: sentBy.userData?.Username,
@@ -29,11 +42,27 @@ export default class Message {
   }
 
   public fromBot(): MessageInterface {
+    const sentBy: UserInterface = {
+      ID: -1,
+      Username: 'Absol',
+      Rank: 'Bot',
+      Auth_Code: 'AbsolBotAuthCode',
+      Avatar: '/Avatars/Custom/3.png',
+    };
+
+    Log.toDatabase(
+      this.message,
+      sentBy,
+      this.sentOn,
+      this.isPrivate,
+      this.isPrivateTo
+    );
+
     return {
-      userID: -1,
-      userName: 'Absol',
-      userRank: 'Bot',
-      userAvatar: '/Avatars/Custom/3.png',
+      userID: sentBy.ID,
+      userName: sentBy.Username,
+      userRank: sentBy.Rank,
+      userAvatar: sentBy.Avatar,
       messageText: this.message,
       isPrivate: this.isPrivate,
       isPrivateTo: this.isPrivateTo,
